f96372688b182a2ad19406b8a76237eca408a08b
Jonathan Bessa <jonathan.bessa@ethifinance.com>
2026-04-23 12:13:49 +0200

    chore(config): setup eslint perfectionist and vscode auto-format

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,20 @@
-// @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt({
+  rules: {
+    'perfectionist/sort-imports': ['error', {
+      type: 'alphabetical',
+      order: 'asc',
+      groups: [
+        'type',
+        ['builtin', 'external'],
+        'internal',
+        ['parent', 'sibling', 'index'],
+      ],
+    }],
+    'perfectionist/sort-vue-attributes': 'error',
+    'perfectionist/sort-interfaces': 'error',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'no-undef': 'error'
+  }
+})
